fe8a00c43b602ced0fe1e6c58893fa0a86e192bb
bismarkagyare <bismarkagyare54@gmail.com>
2024-01-09 16:33:16 +0000

    Create folder for routes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,29 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import authRouter from './routes/auth.js';
+import authHotels from './routes/hotels.js';
+import authRooms from './routes/rooms.js';
+import authUsers from './routes/users.js';
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+//middlewares
+app.use('/api/auth', authRouter);
+
 app.listen('8000', () => {
+  connect();
   console.log('connected to server');
 });
---
+++ server/routes/auth.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('this is auth endpoint');
+});
+
+router.get('/register', (req, res) => {
+  res.send('this is registration endpoint');
+});
+
+export default router;
