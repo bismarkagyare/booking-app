bc14a1529965c8a3450d09c790d62472c8177b16
bismarkagyare <bismarkagyare54@gmail.com>
2024-01-11 13:55:59 +0000

    Implement middlewares

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import authRouter from './routes/auth.js';
-import authHotels from './routes/hotels.js';
-import authRooms from './routes/rooms.js';
-import authUsers from './routes/users.js';
+import hotelsRouter from './routes/hotels.js';
+import roomsRouter from './routes/rooms.js';
+import usersRouter from './routes/users.js';
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
@@ -21,7 +21,12 @@ app.get('/', (req, res) => {
 });
 
 //middlewares
+app.use(express.json());
+
 app.use('/api/auth', authRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/hotels', hotelsRouter);
+app.use('/api/rooms', roomsRouter);
 
 app.listen('8000', () => {
   connect();
