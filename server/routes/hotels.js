1b491388949c3f7c15482189cbe7c866537fef47
bismarkagyare <bismarkagyare54@gmail.com>
2024-02-01 20:52:23 +0000

    implement get all hotels endpoint

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -39,4 +39,24 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+// GET
+router.get('/:id', async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// GET ALL
+router.get('/', async (req, res) => {
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json(hotels);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 export default router;
