48e3bf6d78ad38311ce6688830e579708a36e458
bismarkagyare <bismarkagyare54@gmail.com>
2024-02-04 12:24:54 +0000

    hash user password using bycrypt

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,11 +1,15 @@
 import User from '../models/User.js';
+import bycrypt from 'bcryptjs';
 
 export const register = async (req, res, next) => {
   try {
+    const salt = bycrypt.genSaltSync(10);
+    const hash = bycrypt.hashSync(req.body.password, salt);
+
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hash,
     });
 
     await newUser.save();
