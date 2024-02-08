00eb17aff02dd8d94387454fad77f6431192a6f8
bismarkagyare <bismarkagyare54@gmail.com>
2024-02-08 21:26:11 +0000

    implement login functionality

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from '../models/User.js';
 import bycrypt from 'bcryptjs';
+import { createError } from '../utils/error.js';
 
 export const register = async (req, res, next) => {
   try {
@@ -14,6 +15,29 @@ export const register = async (req, res, next) => {
 
     await newUser.save();
     res.status(201).send('User created successfully');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne(req.body.username);
+    if (!user) {
+      return createError(404, 'User not found');
+    }
+
+    const isPasswordCorrect = await bycrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!isPasswordCorrect) {
+      return createError(404, 'Password incorrect');
+    }
+
+    const { password, isAdmin, ...otherDetails } = user._doc;
+    res.status(200).json({ ...otherDetails });
   } catch (error) {
     next(error);
   }
