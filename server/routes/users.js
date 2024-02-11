76386e62d5199912b43b93d7f6af704f22974404
bismarkagyare <bismarkagyare54@gmail.com>
2024-02-11 19:10:55 +0000

    create and update routes for users

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,13 +1,24 @@
 import express from 'express';
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  getAllUsers,
+} from '../controllers/user.js';
+import { verifyUser, verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('this is user endpoint');
-});
+// UPDATE
+router.put('/:id', verifyUser, updateUser);
 
-router.get('/register', (req, res) => {
-  res.send('this is user registration endpoint');
-});
+// DELETE
+router.delete('/:id', verifyUser, deleteUser);
+
+// GET A SINGLE USER
+router.get('/:id', verifyUser, getUser);
+
+// GET ALL
+router.get('/', verifyAdmin, getAllUsers);
 
 export default router;
